925213f81cee12df065b38bce988889fbd476bd7
EmanSan9 <sanna950@gmail.com>
2025-07-17 16:56:45 +0200

    V17

--- script/libreria.js
+++ script/libreria.js
@@ -10,23 +10,25 @@ if (preferiti) {
   arrayRecuperato.forEach((element) => {
     callTheTower(endpointFisso, query, element).then((data) => {
       const divLib = document.getElementById("divLib");
-      const collina = document.createElement("div");
-      collina.innerHTML = `<div class="col-6 col-md-4 col-lg-3 mb-4">
-          <a href="#" class="text-decoration-none">
-            <div class="card h-100">
-              <img
-                src="${data.data[0].album.cover_xl}"
-                alt="Titolo album"
-                class="card-img-top" />
-              <div class="card-body p-2">
-                <h3 class="card-title">${data.data[0].artist.name}</h3>
-              </div>
-            </div>
-          </a>
-        </div>`;
-      divLib.appendChild(collina);
+
+      divLib.innerHTML += `
+  <div class="col-6 col-md-4 col-lg-3 mb-4">
+    <a href="#" class="text-decoration-none">
+      <div class="card h-100">
+        <img
+          src="${data.data[0].album.cover_xl}"
+          alt="Titolo album"
+          class="card-img-top" />
+        <div class="card-body p-2">
+          <h3 class="card-title">${data.data[0].artist.name}</h3>
+        </div>
+      </div>
+    </a>
+  </div>
+`;
+
       let id = "";
-      collina.addEventListener("click", () => {
+      divLib.addEventListener("click", () => {
         callTheTower(endpointFisso, query, data.data[0].artist.name).then(
           (data) => {
             id = data.data[0].artist.id;
