f9029a6e978e57ee1f51a8fb12c4b0057a01676f
EmanSan9 <sanna950@gmail.com>
2025-07-17 09:21:20 +0200

    V12

--- script/home.js
+++ script/home.js
@@ -1,25 +1,9 @@
 import { callTheTower } from "./callTheTower.js";
 
-const input = document.getElementById("searchBar");
-const searchNav = document.getElementById("searchNav");
-const btnSearch = document.getElementById("btnSearch");
-const searchForm = document.getElementById("searchForm");
-
-btnSearch.addEventListener("click", () => {
-  searchNav.classList.toggle("d-none");
-});
-
 //questo serve per cercare, chiamare API e recuperare id per spostarlo su Artist html
 const endpointFisso = "https://striveschool-api.herokuapp.com/api/deezer/";
 const query = "search?q=";
 let id = "";
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  callTheTower(endpointFisso, query, input.value).then((data) => {
-    id = data.data[0].artist.id;
-    cerca(id);
-  });
-});
 
 //funzione Vai a Cercati
 const cerca = function (id) {
